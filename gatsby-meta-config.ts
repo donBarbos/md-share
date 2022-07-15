82e0c1b858c768ce8a4b532b34acc66510554748
donBarbos <donbarbos@proton.me>
2022-07-15 18:38:27 +0300

    add .editorconfig and dirs

---
+++ gatsby-meta-config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "md share",
+  description: `application for share Markdown files`,
+  author: "donBarbos",
+  siteUrl: "https://gatsby-starter-apple.netlify.app",
+  lang: "en",
+  utterances: "donBarbos/md-share",
+  links: {
+    github: "https://github.com/donBarbos/md-share",
+  },
+  icon: "src/images/icon.png",
+}
