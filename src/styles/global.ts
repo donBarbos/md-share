7b88b40fbd98238183cb4c174ac25624bc894839
donBarbos <donbarbos@proton.me>
2022-10-27 23:08:32 +0600

    fix: correct name of Lato fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,7 +13,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family: -apple-system,BlinkMacSystemFont,Loto,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji";
+    font-family: -apple-system,BlinkMacSystemFont,Lato,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji";
     font-size: 16px;
     font-weight: 500;
     text-align: center;
