45bb83a35dc9bf36654fabc75ca37a8324f43559
donBarbos <donbarbos@proton.me>
2022-10-30 12:42:18 +0600

    refactor: add upload button and remove extra page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,14 @@
 import type { NextPage, GetStaticProps } from 'next'
 import { NextSeo } from 'next-seo'
-import styles from '@styles/Home.module.css'
 import Layout from '@components/layout'
+import UploadButton from '@components/UploadButton'
 
 const Home: NextPage = () => {
   return (
     <>
       <NextSeo title="" description="" />
       <Layout>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-        </div>
+        <UploadButton />
       </Layout>
     </>
   )
