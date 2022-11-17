1983735cb121f614d4ff76609caa05d358eb7f9e
donBarbos <donbarbos@proton.me>
2022-11-17 15:47:14 +0600

    refactor: correct path to components

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import { NextSeo } from 'next-seo'
 import Link from 'next/link'
 import Image from 'next/image'
-import profilePic from '@public/avatar.jpeg'
+import profilePic from '@public/avatar.jpg'
 import Layout from '@components/Layout'
 
 const Contact: NextPage = () => {
@@ -12,31 +12,38 @@ const Contact: NextPage = () => {
       <Layout>
         <h1>Contact</h1>
         <br />
-        <h2>
-          <Image
-            src={profilePic}
-            alt="profile picture of the creator"
-            width={30}
-            height={30}
-            style={{ borderRadius: '30px', position: 'relative', top: '5px' }}
-          />
-          &ensp;donBaros &ndash; creator
-        </h2>
+        <div>
+          <h2>
+            <Image
+              src={profilePic}
+              alt=""
+              width={30}
+              height={30}
+              style={{ borderRadius: '30px', position: 'relative', top: '5px' }}
+            />
+            &ensp;donBaros &ndash; creator
+          </h2>
+          <br />
+          <ul>
+            <li>
+              email: <Link href="mailto:donbarbos@proton.me">donbarbos@proton.me</Link>
+            </li>
+            <li>
+              site: <Link href="https://dontkillkenny.com">dontkillkenny.com</Link>
+            </li>
+            <li>
+              github: <Link href="https://github.com/donBarbos">@donBarbos</Link>
+            </li>
+            <li>
+              telegram: <Link href="https://t.me/donbarbos">@donbarbos</Link>
+            </li>
+          </ul>
+        </div>
         <br />
-        <ul>
-          <li>
-            email: <Link href="mailto:donbarbos@proton.me">donbarbos@proton.me</Link>
-          </li>
-          <li>
-            site: <Link href="https://dontkillkenny.com">dontkillkenny.com</Link>
-          </li>
-          <li>
-            github: <Link href="https://github.com/donBarbos">@donBarbos</Link>
-          </li>
-          <li>
-            telegram: <Link href="https://t.me/donbarbos">@donbarbos</Link>
-          </li>
-        </ul>
+        <div>
+          source code is available at{' '}
+          <Link href="https://github.com/donBarbos/md-share">this link</Link>
+        </div>
       </Layout>
     </>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import { NextSeo } from 'next-seo'
 import Layout from '@components/Layout'
-import UploadButton from '@components/UploadButton'
+import UploadForm from '@components/UploadForm'
 
 const Home: NextPage = () => {
   return (
@@ -9,8 +9,7 @@ const Home: NextPage = () => {
       <NextSeo title="" description="" />
       <Layout>
         <h1>Just Upload Your Markdown Note</h1>
-        <br />
-        <UploadButton />
+        <UploadForm />
       </Layout>
     </>
   )
