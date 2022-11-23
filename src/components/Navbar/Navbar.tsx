a97867ffeefa39fe2bfe05bb6ef41347da0a3d99
donBarbos <donbarbos@proton.me>
2022-11-23 18:32:59 +0600

    refactor: make map from headers

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,6 +5,12 @@ import styles from './styles.module.css'
 import ThemeToggle from '@components/ThemeToggle'
 import BurgerMenu from '@components/BurgerMenu'
 
+const items = [
+  { href: '/', label: 'Home' },
+  { href: '/about', label: 'About' },
+  { href: '/contact', label: 'Contact' },
+]
+
 const Navbar = () => {
   const location = useRouter().pathname.toString()
   const [isActive, setActive] = useState(false)
@@ -15,38 +21,21 @@ const Navbar = () => {
         .mdShare
       </a>
       <ul className={isActive ? `${styles.navbar__menu} ${styles.active}` : styles.navbar__menu}>
-        <li className={styles.navbar__item}>
-          <Link
-            href="/"
-            className={
-              location == '/' ? `${styles.navbar__link} ${styles.active}` : styles.navbar__link
-            }
-          >
-            Home
-          </Link>
-        </li>
-        <li className={styles.navbar__item}>
-          <Link
-            href="/about"
-            className={
-              location == '/about' ? `${styles.navbar__link} ${styles.active}` : styles.navbar__link
-            }
-          >
-            About
-          </Link>
-        </li>
-        <li className={styles.navbar__item}>
-          <Link
-            href="/contact"
-            className={
-              location == '/contact'
-                ? `${styles.navbar__link} ${styles.active}`
-                : styles.navbar__link
-            }
-          >
-            Contact
-          </Link>
-        </li>
+        {items.map((item) => (
+          /* Use the `active` state to conditionally style the active item. */
+          <li className={styles.navbar__item} key={item.href}>
+            <Link
+              href={item.href}
+              className={
+                location === item.href
+                  ? `${styles.navbar__link} ${styles.active}`
+                  : styles.navbar__link
+              }
+            >
+              {item.label}
+            </Link>
+          </li>
+        ))}
         <li className={styles.navbar__item}>
           <ThemeToggle />
         </li>
