44504e85f0cd66da67df81f8f89c4fdabcd2036c
donBarbos <donbarbos@proton.me>
2022-11-23 12:54:04 +0600

    feat: add next-wpa to config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   images: {
     unoptimized: true,
   },
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
-}
+})
 
 module.exports = nextConfig
