72e97aceb4ccb980a96fee0ac875b329ff92dc0c
donBarbos <donbarbos@proton.me>
2022-12-29 13:04:39 +0600

    feat: add @svgr/webpack for svgs

--- next.config.js
+++ next.config.js
@@ -13,6 +13,13 @@ const nextConfig = withPWA({
   images: {
     unoptimized: false,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+    })
+    return config
+  },
 })
 
 module.exports = nextConfig
