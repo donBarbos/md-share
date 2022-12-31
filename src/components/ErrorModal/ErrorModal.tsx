ed66f5ed2c77ad9ea78c52880dc098c4d907bd04
donBarbos <donbarbos@proton.me>
2022-12-31 14:08:24 +0600

    feat: add new component ErrorModal

---
+++ src/components/ErrorModal/ErrorModal.tsx
@@ -0,0 +1,20 @@
+import type { ErrorModalProps } from './types'
+import { Modal } from '@components/Modal'
+import Link from 'next/link'
+import styles from './styles.module.css'
+
+const ErrorModal = ({ isActive, setActive, error }: ErrorModalProps) => {
+  return (
+    <Modal isActive={isActive} setActive={setActive}>
+      <h1 className={styles.modal__title}>Ooops! Internal Server Error</h1>
+      <h3 className={styles.modal__subtitle}>Cannot complete your request.</h3>
+      <h3 className={styles.modal__subtitle}>
+        Try to refresh this page of feel free <Link href="./contact">contact us</Link> if the
+        problem persists.
+      </h3>
+      <p className={styles.modal__description}>messege error: {error}</p>
+    </Modal>
+  )
+}
+
+export { ErrorModal }
---
+++ src/components/ErrorModal/types.ts
@@ -0,0 +1,5 @@
+export type ErrorModalProps = {
+  isActive: boolean
+  setActive: (active: boolean) => void
+  error: string
+}
