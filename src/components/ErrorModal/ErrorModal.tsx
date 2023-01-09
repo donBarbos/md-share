93cb08e419fe722fedb83165c62326502a7bf5b5
donBarbos <donbarbos@proton.me>
2023-01-09 14:52:18 +0600

    refactor: set order for heading

--- src/components/ErrorModal/ErrorModal.tsx
+++ src/components/ErrorModal/ErrorModal.tsx
@@ -7,7 +7,7 @@ const ErrorModal = ({ isActive, setActive, error }: ErrorModalProps) => {
   return (
     <Modal isActive={isActive} setActive={setActive}>
       <h1 className={styles.modal__title}>Ooops! Internal Server Error</h1>
-      <h3 className={styles.modal__subtitle}>Cannot complete your request.</h3>
+      <h2 className={styles.modal__subtitle}>Cannot complete your request.</h2>
       <h3 className={styles.modal__subtitle}>
         Try to refresh this page of feel free <Link href="./contact">contact us</Link> if the
         problem persists.
