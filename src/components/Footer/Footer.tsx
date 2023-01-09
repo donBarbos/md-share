17f4b62381c8cd36911290e01bfdf9d14a60cd7b
donBarbos <donbarbos@proton.me>
2023-01-09 14:37:08 +0600

    fix: make unified font

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link'
 import styles from './styles.module.css'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ weight: ['600'], subsets: ['latin'] })
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
+    <footer className={`${styles.footer} ${inter.className}`}>
       © {new Date().getFullYear()}&nbsp;
       <Link href="https://www.dontkillkenny.com">donBarbos</Link>
     </footer>
