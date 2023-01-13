672b81ce4a8a555d44a15fc73e3e300c5a59fbf5
donBarbos <donbarbos@proton.me>
2023-01-13 10:06:17 +0600

    feat: add support focus-visible polyfill

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,13 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import '@styles/global.css'
+import 'focus-visible'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
-      <ThemeProvider enableSystem={true}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider enableSystem={true}>
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
 
