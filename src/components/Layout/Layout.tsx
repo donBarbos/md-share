fde0a966ad89a2f59eb6063e8cde7568ad56ec6e
donBarbos <donbarbos@proton.me>
2023-01-16 15:18:43 +0600

    refactor: correct loading fonts

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,11 @@
 import type { LayoutProps } from './types'
 import { Header } from '@components/Header'
 import { Footer } from '@components/Footer'
-import { Inter } from '@next/font/google'
 import styles from './styles.module.css'
-
-const inter = Inter({ weight: ['400', '500', '600', '700', '800', '900'], subsets: ['latin'] })
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className={`${styles.wrapper} ${inter.className}`}>
+    <div className={styles.wrapper}>
       <Header />
       <main className={styles.main}>{children}</main>
       <Footer />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
+import { Inter } from '@next/font/google'
 import '@styles/global.css'
 import 'focus-visible'
+
+const inter = Inter({ weight: ['400', '500', '600', '700', '800', '900'], subsets: ['latin'] })
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider enableSystem={true}>
+      <style jsx global>{`
+        :root {
+          --font-base: ${inter.style.fontFamily}, sans-serif;
+        }
+      `}</style>
       <Component {...pageProps} />
     </ThemeProvider>
   )
