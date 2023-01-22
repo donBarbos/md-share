e8f684b33e0d9c4cd8b270de736c262f025c1613
donBarbos <donbarbos@proton.me>
2023-01-22 12:23:57 +0600

    feat: add new component spinner

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,12 @@
+import Loader from '@public/svgs/loading-spinning-bubbles.svg'
+import styles from './styles.module.css'
+
+const Spinner = () => {
+  return (
+    <div className={styles.background}>
+      <Loader height={60} width={60} className={styles.loader} />
+    </div>
+  )
+}
+
+export { Spinner }
