1b58186a316df5f3eb1665284dc1d07786608d38
donBarbos <donbarbos@proton.me>
2023-02-12 10:48:07 +0300

    refactor: correct naming

--- src/components/ScrollToTop/ScrollToTop.tsx
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useLayoutEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './styles.module.css'
 
 const ScrollToTop = () => {
   const [showButton, setShowButton] = useState(false)
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     let lastScrollPos = 0
     const handleScrollEvent = () => {
       const currentScrollPos = window.pageYOffset
@@ -36,9 +36,9 @@ const ScrollToTop = () => {
     >
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="black" role="presentation">
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
-          clip-rule="evenodd"
+          clipRule="evenodd"
         />
       </svg>
     </button>
