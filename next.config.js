e11e2e3700bf733eb60f81cdc4289f29a1efbd9b
donBarbos <donbarbos@proton.me>
2023-02-14 11:21:18 +0300

    feat: add HTTP headers to app

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+]
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
@@ -28,6 +47,15 @@ const nextConfig = withPWA({
       use: [{ loader: '@svgr/webpack', options: { icon: true } }],
     })
     return config
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
   },
 })
 
