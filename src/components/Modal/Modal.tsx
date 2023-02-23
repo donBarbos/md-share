ecff13bb64a2739da5ab30e4d26a812c3b25ecf9
donBarbos <donbarbos@proton.me>
2023-02-23 21:42:37 +0300

    refactor: configure tabindex for modal window

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,11 +1,44 @@
 import type { ModalProps } from './types'
+import { useRef, useEffect } from 'react'
 import styles from './styles.module.css'
 
 const Modal = ({ isActive, setActive, children }: ModalProps) => {
+  const modalRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (isActive) {
+      modalRef.current?.focus()
+    }
+  }, [isActive])
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === 'Tab') {
+      const focusableElements =
+        modalRef.current?.querySelectorAll<HTMLElement>(
+          'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])',
+        ) || []
+      const firstElement = focusableElements[0]
+      const lastElement = focusableElements[focusableElements.length - 1]
+
+      if (event.shiftKey && document.activeElement === firstElement) {
+        event.preventDefault()
+        lastElement.focus()
+      } else if (!event.shiftKey && document.activeElement === lastElement) {
+        event.preventDefault()
+        firstElement.focus()
+      }
+    } else if (event.key === 'Escape') {
+      setActive(false)
+    }
+  }
+
   return (
     <div
       className={isActive ? `${styles.modal} ${styles.active}` : styles.modal}
       onClick={() => setActive(false)}
+      onKeyDown={handleKeyDown}
+      ref={modalRef}
+      tabIndex={isActive ? 0 : -1}
       role="dialog"
       aria-modal="true"
     >
